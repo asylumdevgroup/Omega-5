131c7a302f78fffa28cddb567bfb6c4e76de39c5
Nora Hanegan <norahanegan@gmail.com>
2021-08-18 13:06:07 -0400

    Commit New Changes

--- secrets.ps1
+++
@@ -1,8 +0,0 @@
-# Settings for the ModpackUploader
-# For details/help see: https://github.com/NillerMedDild/ModpackUploader
-
-# =====================================================================//
-#  CURSEFORGE ACCOUNT SETTINGS
-# =====================================================================//
-
-$CURSEFORGE_TOKEN = "bdf1037d-b9c0-4948-ba07-58567b0742f4"
--- tools/GetGameVersions.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location ".."
-. .\secrets.ps1
-
-curl -H X-Api-Token:$CURSEFORGE_TOKEN https://minecraft.curseforge.com/api/game/versions >> GameVersions.json
