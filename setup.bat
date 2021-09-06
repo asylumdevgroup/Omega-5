ca489c1f1046226ff4582667741df476d92eed29
Nora Hanegan <norahanegan@gmail.com>
2021-09-06 14:36:23 -0400

    1.1.6.1

--- setup.bat
+++ setup.bat
@@ -2,11 +2,11 @@
 
 type NUL > .git/hooks/post-merge
 echo #!/bin/sh > .git/hooks/post-merge
-echo java -jar InstanceSync.jar >> .git/hooks/post-merge
+echo java -jar ModpackDownloader-cli-0.6.1.jar >> .git/hooks/post-merge
 
 echo Done setting up hooks
-echo Running InstanceSync
+echo Running Modpack Downloader
 
-java -jar InstanceSync.jar
+java -jar ModpackDownloader-cli-0.6.1.jar
 
 pause
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 echo "#!/bin/sh" > .git/hooks/post-merge
-echo "java -jar InstanceSync.jar" >> .git/hooks/post-merge
+echo "java -jar ModpackDownloader-cli-0.6.1.jar" >> .git/hooks/post-merge
 
 echo "Done setting up hooks"
-echo "Running InstanceSync"
+echo "Running Modpack Downloader"
 
-java -jar InstanceSync.jar
+java -jar ModpackDownloader-cli-0.6.1.jar
